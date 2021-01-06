3ee3624a4ff150fd760df1154862f00b656230e9
James Rock <63159424+Vampire420@users.noreply.github.com>
2021-01-06 18:45:36 +0100

    Update locator.sh

--- locator.sh
+++ locator.sh
@@ -15,7 +15,6 @@ elif [ -n "$1" ]; then
 
 #AKCE
 
-clear
 curl -l -s https://ipapi.co/"$1"/json/ > scan
 grep -vE 'region_code|country_code|country_code_iso3|country_capital|country_tld|continent_code|postal|timezone|utc_offset|currency_name|languages|country_area|country_population|asn|{|}' scan > scan_clear
 rm scan
